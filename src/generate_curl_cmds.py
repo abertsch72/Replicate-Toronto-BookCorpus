5664e7b02e3bb67c8b83371d8ab047bcd125d160
Amanda Bertsch <abertsch72@gmail.com>
2022-04-20 20:26:29 -0400

    add more books

--- src/generate_curl_cmds.py
+++ src/generate_curl_cmds.py
@@ -4,16 +4,20 @@ def get_name(url):
     url = url.split('/')[-1].split('.')[0]
     return url
 
-def gen_commands(filename, max):
+def gen_commands(filename, max, outfile = None):
     category = filename.split('-')[-1].split('.')[0]
     with open(filename) as f:
         urls = [line.strip() for line in f.readlines()]
-        urls = [url for url in urls if not os.path.exists(f"../data/{category}/{get_name(url)}.txt")]
+        urls = [url for url in urls if not os.path.exists(f"data/{category}/{get_name(url)}.txt")]
     print(len(urls))
     for url in urls[:max]:
         print(f"curl {url} -o data/{category}/{get_name(url)}.txt")
+    if outfile:
+        with open(outfile, 'w') as f:
+            f.write("#!/bin/bash\n")
+            f.writelines([f"curl {url} -o data/{category}/{get_name(url)}.txt\n" for url in urls[:max]])
 
-gen_commands("../data/book_urls-1206.txt", 50)
+gen_commands("data//book_urls-1206.txt", 50, "getdata.sh")
 
 
 def remove_throttled(folder):
@@ -25,4 +29,4 @@ def remove_throttled(folder):
 
     print(count)
 
-remove_throttled("../data/1206")
+remove_throttled("data/1206")
--- src/get_book_urls.py
+++ src/get_book_urls.py
@@ -189,9 +189,9 @@ def main(this_category):
 
 if __name__ == "__main__":
 
-    categories = [1206] #883, 1206, 1235, 1213, 892, 1126, 871, 882, 61, 873, 874, 877, 1018, 897]
+    categories = [1235, 1206, 1213, 883, 882]# [1206, 883, 1206, 1235, 1213, 892, 1126, 871, 882, 61, 873, 874, 877, 1018, 897]
 
     num_attempts = 1
-    for category in categories:
-        for i in range(num_attempts):
-            main(category)
+    for i in range(num_attempts):
+        for category in categories:
+                main(category)
